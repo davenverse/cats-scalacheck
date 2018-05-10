29b4f4c84034b99a6beee74aacb6d758290a1aac
Chris Davenport <chris@christopherdavenport.tech>
2018-05-10 15:14:53 -0400

    Initial Commit

---
+++ src/main/scala/org/scalacheck/cats/implicits/implicits.scala
@@ -0,0 +1,5 @@
+package org.scalacheck.cats
+
+import org.scalacheck.cats.instances.GenInstances
+
+package object implicits extends GenInstances
---
+++ src/main/scala/org/scalacheck/cats/instances/gen.scala
@@ -0,0 +1,46 @@
+package org.scalacheck.cats.instances
+
+import cats._
+import cats.implicits._
+import org.scalacheck.Gen
+
+object GenInstances extends GenInstances
+
+trait GenInstances extends GenInstances1
+
+  sealed private[instances] trait GenInstances1 extends GenInstances0 {
+    implicit val genInstances : Monad[Gen] with SemigroupK[Gen] = new Monad[Gen] with Alternative[Gen] {
+      // Members declared in cats.Applicative
+      override def pure[A](x: A): Gen[A] = Gen.const(x)
+
+      // Members declared in cats.FlatMap
+      override def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] = fa.flatMap(f)
+      override def tailRecM[A, B](a: A)(f: A => Gen[Either[A,B]]): Gen[B] = Gen.tailRecM(a)(f)
+
+      override def combineK[A](x: Gen[A], y: Gen[A]): Gen[A] = Gen.gen{ (params, seed) => 
+        val xGen = x.doApply(params, seed)
+        if (xGen.retrieve.isDefined) xGen
+        else y.doApply(params, seed)
+      }
+
+      override def empty[A]: Gen[A] = Gen.fail
+    }
+
+    implicit def genMonoid[A: Monoid]: Monoid[Gen[A]] = new Monoid[Gen[A]]{
+      override def empty: Gen[A] = Gen.const(Monoid[A].empty)
+      override def combine(x: Gen[A], y: Gen[A]) = for {
+        xa <- x
+        ya <- y
+      } yield xa |+| ya
+    }
+
+  }
+
+  sealed private[instances] trait GenInstances0 {
+    implicit def genSemigroup[A: Semigroup]: Semigroup[Gen[A]] = new Semigroup[Gen[A]]{
+      override def combine(x: Gen[A], y: Gen[A]) = for {
+        xa <- x
+        ya <- y
+      } yield xa |+| ya
+    }
+  }
